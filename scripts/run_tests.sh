e7aa4ae6949e1076720aa04d969e240189f1cacd
solo2112 <dawitabrham0021@gmail.com>
2024-08-15 08:36:17 +0300

    updated(task_manager_api): ci go version updated

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,15 +3,21 @@
 # Exit on error
 set -e
 
-# List of packages to exclude (adjust as needed)
+# Change to the directory where the script is located
+cd "$(dirname "$0")/.."
+
+# Module path
+module_path="github.com/solo21-12/A2SV_back_end_track/task_seven"
+
+# List of packages to exclude (full path format)
 exclude_packages=(
-    "task_seven/tests/task_tests/repo"
-    "task_seven/tests/user_tests/repo"
-    "task_seven/tests/constants"
+    "github.com/solo21-12/A2SV_back_end_track/tree/main/task_seven/tests/task_tests/repo"
+    "github.com/solo21-12/A2SV_back_end_track/tree/main/task_seven/tests/user_tests/repo"
+    "github.com/solo21-12/A2SV_back_end_track/tree/main/task_seven/tests/constants"
 )
 
 # Find all packages with .go files in the tests directory
-all_packages=$(go list ../tests/...)
+all_packages=$(go list ./tests/...)
 
 # Filter out the excluded packages
 for exclude in "${exclude_packages[@]}"; do
@@ -26,5 +32,11 @@ echo "Test packages to run:"
 echo $test_packages_list
 
 # Run tests for the filtered packages
-echo "Running tests..."
-go test $test_packages_list
+if [ -n "$test_packages_list" ]; then
+    echo "Running tests..."
+    go test $test_packages_list
+else
+    echo "No test packages to run."
+fi
+
+
